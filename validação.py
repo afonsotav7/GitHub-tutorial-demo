5172d3cdeb5ca43ff40a6ed3de317d0954e34f90
Afonso Sousa <1250718@isep.ipp.pt>
2025-11-30 22:49:55 +0000

    Update validação.py

--- validação.py
+++ validação.py
@@ -1,9 +1,5 @@
 # Funções de validação de formulário
 
-def validar_email(email):
-    """Valida se o email é válido"""
-    return "@" in email and "." in email
-
 def validar_telefone(telefone):
     """Valida se o telefone é válido"""
     # TODO: Implementar validação
